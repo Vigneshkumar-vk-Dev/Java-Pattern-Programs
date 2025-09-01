c4a9f56e050cb18d77c460ded9aba28fb47ea423
Vigneshkumar-vk-Dev <vigneshvikneshvk@gmail.com>
2025-09-01 20:37:49 +0530

    Added Java Pattern Programs

---
+++ PlusShape.java
@@ -0,0 +1,23 @@
+public class PlusShape {
+    public static void main(String[] args) {
+        int n = 5; // odd number for symmetry
+        int mid = (n / 2) + 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == mid || j == mid) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+/* Output:
+      *     
+      *
+  * * * * *
+      *
+      *
+ */
